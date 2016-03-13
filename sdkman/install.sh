159eab8be48d65c81eb0a025ef52e8fdd248d92f
whamondg <gordon.whamond@googlemail.com>
2016-03-13 08:41:05 +0000

    update sdkman if already installed

--- sdkman/install.sh
+++ sdkman/install.sh
@@ -2,18 +2,18 @@
 #
 # SDKMAN
 #
-# This installs SDKMAN if it isn't already installed
+# Installs SDKMAN if it isn't already installed
 
 INIT_FILE="$HOME/.sdkman/bin/sdkman-init.sh"
-echo $INIT_FILE
 
-if [ ! -f $INIT_FILE ];
+if [ ! -s $INIT_FILE ];
 then
   echo "  Installing sdkman"
   curl -s get.sdkman.io | bash
-  
-  echo "  Sourcing the sdkman init file"
-  source "$HOME/.sdkman/bin/sdkman-init.sh"
+else
+  echo "  SDKMAN is already installed"
+  source $INIT_FILE
+  sdk selfupdate
 fi
 
 exit 0
