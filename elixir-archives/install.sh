5ba63d655320b31ad38b6fc62091cdc65cc01bcc
whamondg <gordon.whamond@googlemail.com>
2016-05-23 17:43:39 +0100

    Install phoenix

---
+++ elixir-archives/install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# Elixir Tools
+#
+# Installs Elixir tooling and Mix archives
+
+mix archive.install --force https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez
+
+exit 0
