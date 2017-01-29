b6c9edfb2c9bd78db764b0c248cec839066d898d
Gordon Whamond <gordon.whamond@sky.uk>
2017-01-29 22:58:35 +0000

    renamed elixir archive setup as it is breaking the install

--- elixir-archives/setup.sh
+++ elixir-archives/setup.sh
@@ -1,9 +1,5 @@
-#!/bin/sh
-#
-# Elixir Tools
-#
 # Installs Elixir tooling and Mix archives
 
 mix archive.install --force https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez
 
-exit 0
+echo "Finished installing Mix archives"
